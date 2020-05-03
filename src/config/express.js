70d6cff079e12d5fb2be51691c33a4f1f86d1d48
Hiago <hiago.tech@gmail.com>
2020-05-03 18:11:40 -0300

    eslint configurado

--- src/config/express.js
+++ src/config/express.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const Express = require('express');
 const bodyParser = require('body-parser');
 
 const expressConfig = () => {
-  const app = new express();
-  
-  app.use(bodyParser.json({limit: '5mb'}));
-  app.use(bodyParser.urlencoded({limit: '5mb', extended: true}));
+  const app = new Express();
+
+  app.use(bodyParser.json({ limit: '5mb' }));
+  app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
 
   return app;
 };
