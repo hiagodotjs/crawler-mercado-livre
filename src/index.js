e8a0ae547ffb9f822c12b9ab263665b4c65750e0
Hiago <hiago.tech@gmail.com>
2020-05-03 23:39:24 -0300

    ignorar console log do start da api

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ const app = configExpress();
 dotenv.config();
 const { API_PORT } = process.env;
 app.listen(API_PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`API is running on port ${API_PORT}`);
 });
